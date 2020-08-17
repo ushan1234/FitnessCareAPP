355295a03e37223af5de23f12252a3855f58bedd
ushan1234 <prasadheshan@gmail.com>
2020-08-17 08:47:42 +0530

    initial

---
+++ app/src/main/java/comtechnobyte/example/fitnesscareapp/CalorieCalculator.java
@@ -0,0 +1,14 @@
+package comtechnobyte.example.fitnesscareapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalorieCalculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calorie_calculator);
+    }
+}
