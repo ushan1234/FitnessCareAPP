2ab727965e3c36e975ce28b6469701c513ad663c
janith <it19126616@my.sliit.lk>
2020-09-18 20:08:48 +0530

    insert

--- app/src/main/java/comtechnobyte/example/fitnesscareapp/Register.java
+++ app/src/main/java/comtechnobyte/example/fitnesscareapp/Register.java
@@ -1,18 +1,26 @@
 package comtechnobyte.example.fitnesscareapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
 
-    EditText FullName,Age,Date,Username,Password,Repassword;
+    EditText FullName,Email,Date,Username,Password,Repassword;
     Button Register;
     FirebaseAuth fAuth;
     ProgressBar progressbar;
@@ -24,7 +32,7 @@ public class Register extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         FullName= findViewById(R.id.Name);
-        Age=findViewById(R.id.Age);
+        Email=findViewById(R.id.Email);
         Date=findViewById(R.id.Date);
         Username=findViewById(R.id.UName);
         Password=findViewById(R.id.Pwd);
@@ -34,5 +42,50 @@ public class Register extends AppCompatActivity {
 
         fAuth= FirebaseAuth.getInstance();
         progressbar=findViewById(R.id.progressBar2);
+
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email=Email.getText().toString().trim();
+                String password=Password.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    Email.setError("Email is Required.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    Password.setError("Password is Required");
+                    return;
+                }
+                if(password.length()<6){
+                    Password.setError("Password Must be more than 6 Characters.");
+                    return;
+                }
+                progressbar.setVisibility(View.VISIBLE);
+
+                //register the user in firebase
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            Toast.makeText(Register.this,"User Created",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }
+                        else{
+                            Toast.makeText(Register.this, "Error !"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+            }
+        });
     }
 }
