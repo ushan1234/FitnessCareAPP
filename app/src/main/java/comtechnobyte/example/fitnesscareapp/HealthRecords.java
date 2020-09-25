5a74fc6a1787f6c2c4c23a519093537676238dfe
janith <it19126616@my.sliit.lk>
2020-09-25 20:34:11 +0530

    insert

--- app/src/main/java/comtechnobyte/example/fitnesscareapp/HealthRecords.java
+++ app/src/main/java/comtechnobyte/example/fitnesscareapp/HealthRecords.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
@@ -12,7 +13,9 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -67,29 +70,83 @@ public class HealthRecords extends AppCompatActivity {
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
 
                 addHelper addhelper=healthList.get(i);
-                showUpdateDialog(addhelper.getID(), addhelper.getPressure(),addhelper.getSugar(),addhelper.getCholesterol());
+                showUpdateDialog(addhelper.getID(), addhelper.getBlood(), addhelper.getGender(), addhelper.getPressure(),addhelper.getSugar(),addhelper.getCholesterol(),addhelper.getDate());
                 return false;
             }
         });
     }
-    private void showUpdateDialog(String healthID, String pressure, String sugar, String cholesterol){
+    private void showUpdateDialog(final String healthID, final String blood, final String gender, final String pressure, final String sugar, final String cholesterol, final String date){
         AlertDialog.Builder dialogBuilder=new AlertDialog.Builder(this);
         LayoutInflater inflater=getLayoutInflater();
         final View dialogView=inflater.inflate(R.layout.update_dialog,null);
         dialogBuilder.setView(dialogView);
 
-        final TextView textViewpressure=(TextView) dialogView.findViewById(R.id.textViewpre);
+        final Spinner spinner3=(Spinner) dialogView.findViewById(R.id.spinner3);
+        final Spinner spinner4=(Spinner) dialogView.findViewById(R.id.spinner4);
+
+
         final EditText editTextpressre=(EditText) dialogView.findViewById(R.id.editTextpre);
-        final TextView textViewsugar=(TextView) dialogView.findViewById(R.id.textViewsug);
+
         final EditText editTextsugar=(EditText) dialogView.findViewById(R.id.editTextsug);
-        final TextView textViewcholesterol=(TextView) dialogView.findViewById(R.id.textViewcole);
+
         final EditText editTextcholesterol=(EditText) dialogView.findViewById(R.id.editTextcol);
+        final EditText editTextDate=(EditText) dialogView.findViewById(R.id.editTextDate);
         final Button update=(Button) dialogView.findViewById(R.id.button8);
+        final Button delete=(Button) dialogView.findViewById(R.id.delete);
 
         dialogBuilder.setTitle("Updating Record"+ healthID);
 
-        AlertDialog alertDialog=dialogBuilder.create();
+        final AlertDialog alertDialog=dialogBuilder.create();
         alertDialog.show();
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Blood= spinner3.getSelectedItem().toString();
+                String Gender= spinner4.getSelectedItem().toString();
+                String Pressure= editTextpressre.getText().toString().trim();
+                String Sugar= editTextsugar.getText().toString().trim();
+                String Cholesterol=editTextcholesterol.getText().toString().trim();
+                String Date=editTextDate.getText().toString().trim();
+
+                if(TextUtils.isEmpty(Pressure)){
+                    editTextpressre.setError("This Field Is Required");
+                    return;
+                }
+                if(TextUtils.isEmpty(Sugar)){
+                    editTextsugar.setError("This Field Is Required");
+                    return;
+                }
+
+                updateHealth(healthID,Blood,Gender,Pressure,Sugar,Cholesterol,Date);
+                alertDialog.dismiss();
+
+            }
+        });
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteHealth(healthID);
+                alertDialog.dismiss();
+            }
+        });
+
+
+
+    }
+    private void deleteHealth(String healthID){
+        DatabaseReference dHealth=FirebaseDatabase.getInstance().getReference("health").child(healthID);
+        dHealth.removeValue();
+        Toast.makeText(this, "Record is Deleted", Toast.LENGTH_LONG).show();
+
+    }
+
+    private boolean updateHealth(String id, String blood, String gender, String pressure, String sugar, String cholesterol, String date){
+        DatabaseReference databaseReference =FirebaseDatabase.getInstance().getReference("health").child(id);
+        addHelper addhelper=new addHelper(id,blood,gender,pressure,sugar,cholesterol,date);
+        databaseReference.setValue(addhelper);
+        Toast.makeText(this,"Health Record Updated Successfully",Toast.LENGTH_LONG).show();
+        return true;
 
     }
 }
