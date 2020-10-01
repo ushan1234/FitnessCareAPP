3cfcd71adb12b5f37cad49c70c7a490bdf5addfc
janith <it19126616@my.sliit.lk>
2020-10-01 22:18:52 +0530

    update

--- app/src/main/java/comtechnobyte/example/fitnesscareapp/ProfileFragment.java
+++ app/src/main/java/comtechnobyte/example/fitnesscareapp/ProfileFragment.java
@@ -1,7 +1,9 @@
 package comtechnobyte.example.fitnesscareapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -14,6 +16,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
+
+    CardView health;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -49,6 +53,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -58,7 +63,17 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v= inflater.inflate(R.layout.fragment_profile, container, false);
+        health=(CardView) v.findViewById(R.id.health);
+        health.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+                return v;
     }
+
 }
